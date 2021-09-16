6031d9009cbb08a61951fc8195c0b68f6c3a345b
Ean Maloney <eanmaloney@gmail.com>
2021-09-16 16:06:43 -0400

    Working on app

--- dashboard/app.py
+++ dashboard/app.py
@@ -14,12 +14,24 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+class schools(db.Model):
+    unitid = db.Column(db.integer)
+    school = db.Column(db.String(100))
+    lat = db.Column(db.String(10))
+    lon = db.Column(db.String(10))
+    region = db.Column(db.String(3))
+    nearest_mlb = db.Column(db.String(50))
+    nearest_mlb_dist = db.Column(db.String(5))
+    nearest_nba = db.Column(db.String(50))
+    nearest_nba_dist = db.Column(db.String(50))
+
+
 
 ### Import class object?? ###
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", value = schools.query.all())
 
 if __name__ == "__main__":
     app.run()    
