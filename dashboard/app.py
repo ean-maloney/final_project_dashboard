de1c4acf88fff38d36ba16268f95d03762736118
Ean Maloney <eanmaloney@gmail.com>
2021-09-16 16:21:52 -0400

    Fixing

--- dashboard/app.py
+++ dashboard/app.py
@@ -14,17 +14,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-## Based on https://pythonbasics.org/flask-sqlalchemy/
-class schools(db.Model):
-    unitid = db.Column(db.integer)
-    school = db.Column(db.String(100))
-    lat = db.Column(db.String(10))
-    lon = db.Column(db.String(10))
-    region = db.Column(db.String(3))
-    nearest_mlb = db.Column(db.String(50))
-    nearest_mlb_dist = db.Column(db.String(5))
-    nearest_nba = db.Column(db.String(50))
-    nearest_nba_dist = db.Column(db.String(50))
+# ## Based on https://pythonbasics.org/flask-sqlalchemy/
+# class schools(db.Model):
+#     unitid = db.Column(db.integer)
+#     school = db.Column(db.String(100))
+#     lat = db.Column(db.String(10))
+#     lon = db.Column(db.String(10))
+#     region = db.Column(db.String(3))
+#     nearest_mlb = db.Column(db.String(50))
+#     nearest_mlb_dist = db.Column(db.String(5))
+#     nearest_nba = db.Column(db.String(50))
+#     nearest_nba_dist = db.Column(db.String(50))
 
 
 
