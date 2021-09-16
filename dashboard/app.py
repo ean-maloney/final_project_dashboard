4908faf910de4571b1e89a2b0a8bd951bf7411b3
Ean Maloney <eanmaloney@gmail.com>
2021-09-16 16:19:15 -0400

    Fixing

--- dashboard/app.py
+++ dashboard/app.py
@@ -32,7 +32,7 @@ class schools(db.Model):
 
 @app.route("/")
 def home():
-    return render_template("index.html", value = schools.query.all())
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()    
